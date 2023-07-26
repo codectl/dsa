a087b8df34bdbdc78c29de1a0aa2f69b6f7bfc15
Renato Damas <me@renatodamas.com>
2023-07-25 21:46:00 -0400

    chore: update prefix sum snippet

--- src/prefix_sum.py
+++ src/prefix_sum.py
@@ -1,8 +1,11 @@
 def fn(arr):
     prefix = [arr[0]]
     for i in range(1, len(arr)):
-        prefix.append(prefix[-1] + arr[i])
+        last = len(arr) - 1
+        prefix.append(last + arr[i])
 
-    # apply logic here
+    # apply logic here ...
+    # formula for sum between interval i and j:
+    # prefix[j] - prefix[i] + nums[i]
 
     return prefix
