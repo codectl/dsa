d40ab2890695a37e4c9e4d2836b0a02292e6aec9
Renato Damas <me@renatodamas.com>
2023-07-27 09:26:58 -0400

    chore: add example to sliding window pattern

--- src/sliding_window_unfixed_length.py
+++ src/sliding_window_unfixed_length.py
@@ -11,3 +11,23 @@ def fn(arr):
         # update ans
 
     return ans
+
+
+# EXAMPLES
+def min_sub_array_len(target: int, nums: list[int]) -> int:
+    """Return the minimal length of a subarray
+    whose sum is greater than or equal to target.
+    If there is no such subarray, return 0 instead.
+    """
+    if sum(nums) < target:
+        return 0
+
+    left = curr = 0
+    ans = len(nums)
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr >= target:
+            ans = min(ans, right - left + 1)
+            curr -= nums[left]
+            left += 1
+    return ans
